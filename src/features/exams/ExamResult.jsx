a447eccf1036034578ff0e3f61a7ee8f9f39c953
indrabasuki <indrbasuki1@gamil.com>
2023-02-27 23:35:16 +0700

    db to db

--- src/features/exams/ExamResult.jsx
+++ src/features/exams/ExamResult.jsx
@@ -1,16 +1,16 @@
-import { Box, Button, Container, Grid, Typography } from '@mui/material'
-import { ArrowBack } from '@mui/icons-material'
-import Api, { getExamScore } from '../../api/Api'
-import React, { useEffect, useMemo } from 'react'
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
+import { ArrowBack } from "@mui/icons-material";
+import Api, { getExamScore } from "../../api/Api";
+import React, { useEffect, useMemo } from "react";
 
-import { useNavigate, useParams } from 'react-router-dom'
-import NotFound from '../../components/Error/NotFound'
-import MemberProfile from './Components/MemberProfile'
-import MemberExamResult from './Components/MemberExamResult'
-import useAuth from '../../hooks/useAuth'
+import { useNavigate, useParams } from "react-router-dom";
+import NotFound from "../../components/Error/NotFound";
+import MemberProfile from "./Components/MemberProfile";
+import MemberExamResult from "./Components/MemberExamResult";
+import useAuth from "../../hooks/useAuth";
 
 const ExamResultInfo = ({ data }) => {
-  console.log('result: ', data)
+  console.log("result: ", data);
   return (
     <Grid container spacing={3}>
       <Grid item lg={4} md={6} xs={12}>
@@ -19,62 +19,70 @@ const ExamResultInfo = ({ data }) => {
       <Grid item lg={8} md={6} xs={12}>
         <MemberExamResult data={data} />
       </Grid>
+
+     
+        <iframe
+          src="https://admin.ielts-goldenenglish.com/public/api/certificate/39a37e3a-1471-4dab-a548-972bd6135eb7"
+          width="100%"
+          height="100%"
+        />
+   
     </Grid>
-  )
-}
+  );
+};
 
 const UserExamResult = () => {
-  const [data, setData] = React.useState({})
-  const [errorData, setErrorData] = React.useState({})
-  const { auth } = useAuth()
-  const [accessToken, setAccessToken] = React.useState(auth.access_token)
-  const { id } = useParams()
-  const navigate = useNavigate()
+  const [data, setData] = React.useState({});
+  const [errorData, setErrorData] = React.useState({});
+  const { auth } = useAuth();
+  const [accessToken, setAccessToken] = React.useState(auth.access_token);
+  const { id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const FetchData = async () => {
       try {
-        const response = await getExamScore(accessToken, id)
+        const response = await getExamScore(accessToken, id);
 
-        setData(response)
+        setData(response);
       } catch (error) {
-        setErrorData(error?.response.status)
+        setErrorData(error?.response.status);
       }
-    }
-    FetchData()
-  }, [data?.['exam-list']?.id])
+    };
+    FetchData();
+  }, [data?.["exam-list"]?.id]);
 
-  let content
+  let content;
 
   if (errorData === 404) {
-    content = <NotFound />
+    content = <NotFound />;
   }
 
   if (data?.id) {
     content = (
-      <Box className='px-2 py-4'>
+      <Box className="px-2 py-4">
         <Box>
           <Button
             onClick={() => navigate(-1)}
-            variant='text'
+            variant="text"
             startIcon={<ArrowBack />}
-            size='small'
+            size="small"
           >
             Kembali
           </Button>
         </Box>
 
-        <Container maxWidth='lg'>
-          <Typography sx={{ mb: 3 }} variant='h4'>
+        <Container maxWidth="lg">
+          <Typography sx={{ mb: 3 }} variant="h4">
             Hasil Tes
           </Typography>
           <ExamResultInfo data={data} />
         </Container>
       </Box>
-    )
+    );
   }
 
-  return content
-}
+  return content;
+};
 
-export default UserExamResult
+export default UserExamResult;
