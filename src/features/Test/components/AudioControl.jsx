fe1baca542c80538d1e1ff047f9fcf02fdbdcb21
spaceark7 <spacearkchief@gmail.com>
2023-03-12 15:48:11 +0700

    audio recorder version 1 without time limiter

--- src/features/Test/components/AudioControl.jsx
+++ src/features/Test/components/AudioControl.jsx
@@ -1,3 +1,5 @@
+import { Mic, Stop } from '@mui/icons-material'
+import { Box, Button, IconButton } from '@mui/material'
 import React, { useState } from 'react'
 
 const AudioControl = ({
@@ -16,33 +18,65 @@ const AudioControl = ({
   }
 
   return (
-    <div>
-      <h2>Audio Recorder</h2>
+    <Box>
       <main>
-        <div className='audio-controls'>
-          {!permission ? (
-            <button onClick={getPermission} type='button'>
-              Get Microphone
-            </button>
+        <div className='audio-controls transition-all duration-500 ease-out'>
+          {!permission && !audio ? (
+            <Button onClick={getPermission} variant='contained' color='info'>
+              Open Microphone
+            </Button>
+          ) : !hasRecording && permission && recordingStatus === 'inactive' ? (
+            <Box>
+              <IconButton
+                className='h-20 w-20 shadow-lg active:scale-90'
+                onClick={handleStartRecording}
+                color='info'
+              >
+                <Mic className='h-10 w-10' />
+              </IconButton>
+            </Box>
+          ) : recordingStatus === 'recording' ? (
+            <Box>
+              <IconButton
+                onClick={stopRecording}
+                className='h-20 w-20 shadow-lg active:scale-90'
+                color='error'
+              >
+                <Stop className='h-10 w-10' />
+              </IconButton>
+            </Box>
           ) : null}
-          {!hasRecording && permission && recordingStatus === 'inactive' ? (
-            <button onClick={handleStartRecording} type='button'>
-              Start Recording
-            </button>
-          ) : null}
-          {recordingStatus === 'recording' ? (
-            <button onClick={stopRecording} type='button'>
-              Stop Recording
-            </button>
-          ) : null}
+          {/* {!hasRecording && permission && recordingStatus === 'inactive' ? (
+            <Box>
+              <IconButton
+                className='h-20 w-20 shadow-lg active:scale-90'
+                onClick={handleStartRecording}
+                color='info'
+              >
+                <Mic className='h-10 w-10' />
+              </IconButton>
+            </Box>
+          ) : null} */}
+          {/* {recordingStatus === 'recording' ? (
+            <Box>
+              <IconButton
+                onClick={stopRecording}
+                className='h-20 w-20 shadow-lg active:scale-90'
+                color='error'
+              >
+                <Stop className='h-10 w-10' />
+              </IconButton>
+            </Box>
+          ) : null} */}
         </div>
+        {console.log(audio)}
         {audio ? (
           <div className='audio-player'>
             <audio src={audio} controls></audio>
           </div>
         ) : null}
       </main>
-    </div>
+    </Box>
   )
 }
 
--- src/features/Test/components/AudioRecorder.jsx
+++ src/features/Test/components/AudioRecorder.jsx
@@ -1,9 +1,11 @@
+import { Box, Card, Typography } from '@mui/material'
 import { useState, useRef, useEffect, useCallback } from 'react'
+import useAnswers from '../../../hooks/useAnswers'
 import AudioControl from './AudioControl'
 
 const mimeType = 'audio/webm'
 
-const AudioRecorder = ({ maxRecordingTime = 10 }) => {
+const AudioRecorder = ({ maxRecordingTime = 10, data, addAnswer }) => {
   const [permission, setPermission] = useState(false)
   const mediaRecorder = useRef(null)
   const [recordingStatus, setRecordingStatus] = useState('inactive')
@@ -11,6 +13,31 @@ const AudioRecorder = ({ maxRecordingTime = 10 }) => {
   const [audio, setAudio] = useState(null)
   const [audioChunks, setAudioChunks] = useState([])
   const canvasRef = useRef(null)
+
+  const { answers } = useAnswers()
+
+  const [contextAnswer, setContextAnswer] = useState(() => {
+    return (
+      answers.find((item) => item.id === data.id) ?? {
+        id: data.id,
+        answer: '',
+      }
+    )
+  })
+
+  useEffect(() => {
+    const answer = answers.find((item) => item.id === data.id) ?? {
+      id: data.id,
+      answer: '',
+    }
+    setContextAnswer(answer)
+  }, [answers, data.id])
+
+  useEffect(() => {
+    if (contextAnswer.answer) {
+      setAudio(contextAnswer.answer)
+    }
+  }, [contextAnswer.answer])
 
   const getMicrophonePermission = async () => {
     if ('MediaRecorder' in window) {
@@ -69,10 +96,17 @@ const AudioRecorder = ({ maxRecordingTime = 10 }) => {
         const base64String = reader.result
 
         setAudio(base64String)
+        addAnswer({ id: data.id, answer: base64String })
         setAudioChunks([])
       }
     }
-  }, [audioChunks])
+  }, [audioChunks, data.id, addAnswer])
+
+  useEffect(() => {
+    if (stream?.active) {
+      setPermission(true)
+    }
+  }, [stream])
 
   // automatically stop recording after maxRecordingTime
   useEffect(() => {
@@ -85,76 +119,97 @@ const AudioRecorder = ({ maxRecordingTime = 10 }) => {
     return () => clearTimeout(timer)
   }, [recordingStatus, maxRecordingTime, stopRecording])
 
-  // create visualizer
+  // Effect untuk visualisasi audio
   useEffect(() => {
-    let requestAnimationFrameId
-    const canvas = canvasRef.current
-    const canvasCtx = canvas.getContext('2d')
-    const audioCtx = new AudioContext({ sampleRate: 96000 })
-    const analyser = audioCtx.createAnalyser()
+    if (permission) {
+      let requestAnimationFrameId
+      const canvas = canvasRef.current
+      const canvasCtx = canvas.getContext('2d')
+      const audioCtx = new AudioContext({ sampleRate: 96000 })
+      const analyser = audioCtx.createAnalyser()
 
-    let source
+      let source
 
-    if (stream && recordingStatus === 'recording') {
-      source = audioCtx.createMediaStreamSource(stream)
-      source.connect(analyser)
-      //   analyser.connect(audioCtx.destination)
-    }
+      if (stream && recordingStatus === 'recording') {
+        source = audioCtx.createMediaStreamSource(stream)
+        source.connect(analyser)
+        //   analyser.connect(audioCtx.destination)
+      }
 
-    analyser.fftSize = 4096
-    const bufferLength = analyser.frequencyBinCount
-    const dataArray = new Uint8Array(bufferLength)
+      analyser.fftSize = 4096
+      const bufferLength = analyser.frequencyBinCount
+      const dataArray = new Uint8Array(bufferLength)
 
-    const draw = () => {
-      requestAnimationFrameId = requestAnimationFrame(draw)
-      analyser.getByteFrequencyData(dataArray)
-      canvasCtx.fillStyle = 'rgb(255, 255, 255)'
-      canvasCtx.fillRect(0, 0, canvas.width, canvas.height)
-      const barWidth = (canvas.width / bufferLength) * 2.5
-      let barHeight
-      let x = 0
+      const draw = () => {
+        requestAnimationFrameId = requestAnimationFrame(draw)
+        analyser.getByteFrequencyData(dataArray)
+        canvasCtx.fillStyle = 'rgb(255, 255, 255)'
+        canvasCtx.fillRect(0, 0, canvas.width, canvas.height)
+        const barWidth = (canvas.width / bufferLength) * 2.5
+        let barHeight
+        let x = 0
 
-      for (let i = 0; i < bufferLength; i++) {
-        barHeight = dataArray[i] / 2
+        for (let i = 0; i < bufferLength; i++) {
+          barHeight = dataArray[i] / 2
 
-        canvasCtx.fillStyle = `rgb(${barHeight + 100},50,50)`
-        canvasCtx.fillRect(
-          x,
-          canvas.height - barHeight / 2,
-          barWidth,
-          barHeight
-        )
+          canvasCtx.fillStyle = `rgb(${barHeight + 100},50,50)`
+          canvasCtx.fillRect(
+            x,
+            canvas.height - barHeight / 2,
+            barWidth,
+            barHeight
+          )
 
-        x += barWidth + 1
+          x += barWidth + 1
+        }
+      }
+
+      if (recordingStatus === 'recording' && canvas && source) {
+        draw()
+      } else if (recordingStatus === 'stopped' && canvas) {
+        canvasCtx.clearRect(0, 0, canvas.width, canvas.height)
+      }
+
+      return () => {
+        cancelAnimationFrame(requestAnimationFrameId)
+        canvasCtx.clearRect(0, 0, canvas.width, canvas.height)
+        if (source) source.disconnect()
+        audioCtx.close()
       }
     }
-
-    if (recordingStatus === 'recording' && canvas && source) {
-      draw()
-    } else if (recordingStatus === 'stopped' && canvas) {
-      canvasCtx.clearRect(0, 0, canvas.width, canvas.height)
-    }
-
-    return () => {
-      cancelAnimationFrame(requestAnimationFrameId)
-      canvasCtx.clearRect(0, 0, canvas.width, canvas.height)
-      if (source) source.disconnect()
-      audioCtx.close()
-    }
-  }, [recordingStatus, canvasRef, stream, stopRecording])
+  }, [recordingStatus, canvasRef, stream, stopRecording, permission])
 
   return (
-    <>
-      <canvas ref={canvasRef} width={300} height={100} />
-      <AudioControl
-        permission={permission}
-        audio={audio}
-        getPermission={getMicrophonePermission}
-        recordingStatus={recordingStatus}
-        startRecording={startRecording}
-        stopRecording={stopRecording}
-      />
-    </>
+    <Card
+      id={data?.id}
+      elevation={0}
+      className='mx-auto mb-8 w-fit px-4 py-2 transition-all duration-300 ease-out'
+    >
+      <Typography mb={4} variant='overline'>
+        Record Answer Part {data?.question_no}
+      </Typography>
+      <Box className='flex items-center justify-center'>
+        <AudioControl
+          permission={permission}
+          audio={audio}
+          getPermission={getMicrophonePermission}
+          recordingStatus={recordingStatus}
+          startRecording={startRecording}
+          stopRecording={stopRecording}
+        />
+
+        {permission ? (
+          <canvas
+            className={`${
+              audio || recordingStatus === 'inactive' ? 'hidden' : 'block'
+            }`}
+            ref={canvasRef}
+            width={300}
+            height={100}
+          />
+        ) : null}
+      </Box>
+    </Card>
   )
 }
 
